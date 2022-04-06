32209cc588d65e99842e3579cde75ff553b67a14
Kristy Nguyen <knyen008@gmail.com>
2022-04-05 20:56:12 -0600

    Create buttons and event handlers

---
+++ main.js
@@ -0,0 +1,28 @@
+//buttons
+var sideButton = document.querySelector("#side");
+var mainDishButton = document.querySelector("#main-dish");
+var dessertButton = document.querySelector("#dessert");
+var letsCookButton = document.querySelector(".lets-cook");
+
+//eventListeners
+sideButton.addEventListener('click', )
+mainDishButton.addEventListener('click', )
+dessertButton.addEventListener('click', )
+letsCookButton.addEventListener('click', randomizeDish)
+
+//functions and eventHandlers
+function randomizeDish() {
+  // if (value === "side") {
+  //   return random side dish;
+  // } else if (value === "main") {
+  //   return random main dish;
+  // } else if (value === "dessert")
+  //   return random dessert dish;
+};
+
+
+/* 1) Must be able to click button to correct type of dish
+2) Value that results from that will be held while "lets cook" button is pressed
+3) Once "lets cook" is pressed, crockpot will disappear, and text will generate with food.
+
+    */
