1d41663a83e2df141b0a2aad12f9196729a69e22
Kristy Nguyen <knyen008@gmail.com>
2022-04-05 21:37:01 -0600

    Create randomize dishes function

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 //arrays
 var sideDishes = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots",
-"Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"]
+"Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"];
 var mainDishes = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai Yellow Curry",
 "Bibimbap", "Chicken Parmesean", "Butternut Squash Soup", "BBQ Chicken Burgers", "Ramen", "Empanadas",
 "Chicken Fried Rice", "Sheet Pan Fajitas", "Margarita Pizza"];
 var dessertDishes = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler",
 "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova",
-"Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"]
+"Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"];
+
+//page elements
+var stockPot = document.querySelector("svg");
 
 //buttons
 var sideButton = document.querySelector("#side");
@@ -26,16 +29,17 @@ function getRandomIndex(array) {
 };
 
 function randomizeDish() {
-  // if (value === "side") {
-  //   return random side dish;
-  // } else if (value === "main") {
-  //   return random main dish;
-  // } else if (value === "dessert")
-  //   return random dessert dish;
+  if (value === "side") {
+    return sideDishes[getRandomIndex(sideDishes)];
+  } else if (value === "main") {
+    return mainDishes[getRandomIndex(mainDishes)];
+  } else if (value === "dessert") {
+    return dessertDishes[getRandomIndex(dessertDishes)];
+  }
+
 };
 
-
-/* 1) Must be able to click button to correct type of dish
+/* 1) Must be able to click button for correct type of dish
 2) Value that results from that will be held while "lets cook" button is pressed
 3) Once "lets cook" is pressed, crockpot will disappear, and text will generate with food.
 
